0b3ddbc59c484ccbc0eb7cbbd1bde4c791a9f50e
IGabriel30 <ingridgabriel30@hotmail.com>
2024-09-17 22:00:08 -0600

    Se crea DTO de producto

---
+++ AppBlazorWebAss/Data/DTOs/ProductIJGZDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace AppBlazorWebAss.Data.DTOs
+{
+    public class ProductIJGZDTO
+    {
+        public int Id { get; set; }
+
+        public string? NombreIJGZ { get; set; }
+
+        public string? DescripcionIJGZ { get; set; }
+
+        public decimal PrecioIJGZ { get; set; }
+    }
+}
