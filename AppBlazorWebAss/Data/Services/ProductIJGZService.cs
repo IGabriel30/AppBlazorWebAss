2d7e19cdae49217866434659aecc5e92ae841a1f
IGabriel30 <ingridgabriel30@hotmail.com>
2024-09-19 15:19:49 -0600

    servicio

--- AppBlazorWebAss/Data/Services/ProductIJGZService.cs
+++ AppBlazorWebAss/Data/Services/ProductIJGZService.cs
@@ -12,35 +12,37 @@ namespace AppBlazorWebAss.Data.Services
             _httpClient = httpClientFactory.CreateClient("IJGZAPI");
         }
 
-        //metodo para obtener lista de productos
+        // Método para obtener lista de productos
         public async Task<List<ProductIJGZDTO>> GetProductsAsync()
         {
-            //se realiza una solicitud get para obtener la lista de proudctos en formato json
-            var list = await _httpClient.GetFromJsonAsync<List<ProductIJGZDTO>>("products");
-            //devuelve la lista obtenida o una lista vacía si es nula
-            if (list != null)
-                return list;
-            else
-                return new List<ProductIJGZDTO>();
+            // Se realiza una solicitud GET para obtener la lista de productos en formato JSON
+            var response = await _httpClient.GetFromJsonAsync<ProductResponse>("products");
+
+            // Devuelve la lista obtenida o una lista vacía si es nula
+            return response?.Content ?? new List<ProductIJGZDTO>();
         }
 
-        //metodo para crear nuevo producto
+        // Método para crear nuevo producto
         public async Task<ProductIJGZDTO> CreateProductAsync(ProductIJGZDTO producto)
         {
-            //realiza solicitud POST oara crear nuevo producto en formato JSON.
+            // Realiza solicitud POST para crear nuevo producto en formato JSON
             var response = await _httpClient.PostAsJsonAsync("products", producto);
 
-            //asegura q la solicitud sea exitosa sino lanzará una excepción
+            // Asegura que la solicitud sea exitosa; de lo contrario, lanzará una excepción
             response.EnsureSuccessStatusCode();
-            //lee la respuesta n formato JSON y la convierte a un objeto ProductIJGZDTO
+
+            // Lee la respuesta en formato JSON y la convierte a un objeto ProductIJGZDTO
             var productResult = await response.Content.ReadFromJsonAsync<ProductIJGZDTO>();
 
-            //devuelve el producto creado o un nuevo   ProductIJGZDTO si es nulo
-            if(productResult != null) 
-                return productResult;
-            else 
-                return new ProductIJGZDTO();
-
+            // Devuelve el producto creado o un nuevo ProductIJGZDTO si es nulo
+            return productResult ?? new ProductIJGZDTO();
         }
+    }
+
+    // Clase para deserializar la respuesta de productos
+    public class ProductResponse
+    {
+        public List<ProductIJGZDTO> Content { get; set; }
+        // Agrega más propiedades si es necesario
     }
 }
